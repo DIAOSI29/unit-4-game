c4b4bc75ce9891f82afd5d4675ef18f5651fb299
TianQin <qintian29@gmail.com>
2019-10-13 00:50:10 +0800

    spent 3 hours on making progress bar working as expected, problem happened after the first round has finished, where the last button clicked on the last round had unexpectedly impacted on the current round's expression. finally solved this problem by changing the order of button click functions, more specically putting individual button's click function before button click function as a whole.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,10 +22,30 @@ $(document).ready(function() {
   var resultPoints = 0;
   var wins = 0;
   var losses = 0;
+  var redTotal = 0;
+  var blueTotal = 0;
+  var yellowTotal = 0;
+  var greenTotal = 0;
+  var percentageRed = 0;
 
   var bannerText = $("#bannerText");
   bannerText.html();
 
+  function turnIntoPercentage(figure) {
+    return (figure * 100).toFixed(2) + "%";
+  }
+
+  function resetProgressBar() {
+    redTotal = 0;
+    blueTotal = 0;
+    yellowTotal = 0;
+    greenTotal = 0;
+    $(".progress div:eq(0)").css("width", 0);
+    $(".progress div:eq(1)").css("width", 0);
+    $(".progress div:eq(2)").css("width", 0);
+    $(".progress div:eq(3)").css("width", 0);
+    $(".progress").html();
+  }
   console.log("t1");
   //declare function on game start status//
   function gameStart() {
@@ -37,7 +57,12 @@ $(document).ready(function() {
     dYellowPoints = 1 + Math.floor(Math.random() * 12);
     dGreenPoints = 1 + Math.floor(Math.random() * 12);
     resultPoints = 0;
+    redTotal = 0;
+    blueTotal = 0;
+    yellowTotal = 0;
+    greenTotal = 0;
 
+    resetProgressBar();
     bannerText.html("CLICK DIAMONDS TO POINT UP!");
     $("#button-red").attr("data-points", dRedPoints);
     $("#button-blue").attr("data-points", dBluePoints);
@@ -45,25 +70,84 @@ $(document).ready(function() {
     $("#button-green").attr("data-points", dGreenPoints);
   }
 
+  $("#button-red").click(function() {
+    redTotal += parseInt(dRedPoints);
+    console.log(redTotal);
+    percentageRed = redTotal / targetPoints;
+
+    $(".progress div:eq(0)").css("width", turnIntoPercentage(percentageRed));
+    $(".progress").html();
+    if (targetPoints - redTotal - blueTotal - yellowTotal - greenTotal < 0) {
+      resetProgressBar();
+    }
+  });
+
+  $("#button-blue").click(function() {
+    blueTotal += parseInt(dBluePoints);
+    console.log(blueTotal);
+    percentageBlue = blueTotal / targetPoints;
+
+    $(".progress div:eq(1)").css("width", turnIntoPercentage(percentageBlue));
+    $(".progress").html();
+    if (targetPoints - redTotal - blueTotal - yellowTotal - greenTotal < 0) {
+      resetProgressBar();
+    }
+  });
+
+  $("#button-yellow").click(function() {
+    yellowTotal += parseInt(dYellowPoints);
+    console.log(yellowTotal);
+    percentageYellow = yellowTotal / targetPoints;
+
+    $(".progress div:eq(2)").css("width", turnIntoPercentage(percentageYellow));
+    $(".progress").html();
+    if (targetPoints - redTotal - blueTotal - yellowTotal - greenTotal < 0) {
+      resetProgressBar();
+    }
+  });
+
+  $("#button-green").click(function() {
+    greenTotal += parseInt(dGreenPoints);
+    console.log(greenTotal);
+    percentageGreen = greenTotal / targetPoints;
+
+    $(".progress div:eq(3)").css("width", turnIntoPercentage(percentageGreen));
+    $(".progress").html();
+    if (
+      dRedPoints >
+      targetPoints - redTotal - blueTotal - yellowTotal - greenTotal
+    ) {
+      resetProgressBar();
+    }
+  });
+
   $(".btn").click(function() {
     bannerText.html("CLICK DIAMONDS TO POINT UP!");
     resultPoints += parseInt($(this).attr("data-points"));
+
     $("#resultPoints").html(resultPoints);
     $("#resultPoints").addClass("heartBeat");
+    setTimeout(function() {
+      $("#resultPoints").removeClass("heartBeat");
+    }, 900);
+    // var deBounce;
+    // function deBounce() {
+    //   heartBeat = setTimeout($("#resultPoints").addClass("heartBeat"), 1000);
+
     //tried delay but not work//
     // $("#resultPoints").delay(1000).removeClass("heartBeat");
-
-    //if statement for individual button points
 
     if (resultPoints === targetPoints) {
       wins++;
       $("#wins").html(wins);
       gameStart();
+      resetProgressBar();
       bannerText.html("Well done with the diamonds!");
     } else if (resultPoints > targetPoints) {
       losses++;
       $("#losses").html(losses);
       gameStart();
+      resetProgressBar();
       bannerText.html("TRY AGAIN PLEASE!");
     }
   });
